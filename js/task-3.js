6fa5e1fd23376f314e5aeff36cad5d6d9cce4127
Volodymyr Naidonov <vovassikrostov@gmail.com>
2024-08-17 19:57:40 +0300

    add new

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,15 @@
-function checkForSpam(message) {
-  const blockedWord1 = 'spam';
-  const blockedWord2 = 'sale';
-  return (
-    message.toLowerCase().includes(blockedWord1) ||
-    message.toLowerCase().includes(blockedWord2)
-  );
+function filterArray(numbers, value) {
+  let biggerNumberArr = [];
+  for (const number of numbers) {
+    if (number > value) {
+      biggerNumberArr.push(number);
+    }
+  }
+  return biggerNumberArr;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
